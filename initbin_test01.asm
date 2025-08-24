7fc212ee064e55430a82dd5d33e6b317407421b7
CoolGuy <84420090+CoolGuy-0000@users.noreply.github.com>
2025-08-24 17:37:43 +0900

    Update initbin_test01.asm

--- initbin_test01.asm
+++ initbin_test01.asm
@@ -39,7 +39,6 @@ mov rax, [rax+20h]
 ret
 
 FindFuncByName:
-        ; 프로롤그 (비휘발 레지스터 보존 + 16바이트 정렬)
         push    rbp
         mov     rbp, rsp
         push    rbx
@@ -48,75 +47,64 @@ FindFuncByName:
         push    r12
         push    r14
         push    r15
-        sub     rsp, 32                 ; shadow/local (정렬 유지)
+        sub     rsp, 32                 
 
-        mov     rbx, rcx                ; rbx = 모듈 베이스
-        mov     rsi, rdx                ; rsi = 찾을 함수 이름 포인터
+        mov     rbx, rcx                
+        mov     rsi, rdx                
 
-        ; ---------------------------
-        ; NT 헤더 찾기: nt = base + e_lfanew
-        ; ---------------------------
-        mov     eax, DWORD PTR rbx+3Ch    ; e_lfanew
-        lea     r8,  [rbx+rax]              ; r8 = NT headers
+        mov     eax, DWORD PTR rbx+3Ch
+        lea     r8,  [rbx+rax]
 
-        ; ---------------------------
-        ; Export Directory 가져오기
-        ; OptionalHeader.DataDirectory[0] (EXPORT) 의 RVA는
-        ; NT + 24(파일헤더 뒤) + 112(OptHdr64에서 DataDir 시작) = +136
-        ; ---------------------------
-        mov     eax, DWORD PTR r8+136     ; Export Directory RVA
+        mov     eax, DWORD PTR r8+136     
         test    eax, eax
         jz      ffbn_not_found
-        lea     r14, [rbx+rax]              ; r14 = Export Directory (VA)
+        lea     r14, [rbx+rax]             
 
-        ; 자주 쓰는 필드/테이블 주소
-        mov     eax, DWORD PTR r14+24     ; NumberOfNames
+        mov     eax, DWORD PTR r14+24    
         test    eax, eax
         jz      ffbn_not_found
-        mov     r10d, eax                    ; r10d = NumberOfNames
+        mov     r10d, eax                   
 
-        mov     eax, DWORD PTR r14+32     ; AddressOfNames RVA
-        lea     r15, [rbx+rax]               ; r15 = AddressOfNames (VA)
+        mov     eax, DWORD PTR r14+32   
+        lea     r15, [rbx+rax]            
 
-        mov     eax, DWORD PTR r14+36     ; AddressOfNameOrdinals RVA
-        lea     rdi, [rbx+rax]               ; rdi = AddressOfNameOrdinals (VA)
+        mov     eax, DWORD PTR r14+36    
+        lea     rdi, [rbx+rax]            
 
-        ; 루프 인덱스 i = 0 .. NumberOfNames-1
-        xor     r12d, r12d                   ; r12d = i
+        xor     r12d, r12d        
 
 ffbn_search_loop:
         cmp     r12d, r10d
         jae     ffbn_not_found
 
-        ; namePtr = base + ((DWORD*)AddressOfNames)[i]
         mov     eax, DWORD PTR r15 + r12*4
-        lea     rdx, [rbx+rax]               ; rdx = 현재 이름 포인터
-        mov     rcx, rsi                     ; rcx = 찾을 이름 포인터
+        lea     rdx, [rbx+rax]         
+        mov     rcx, rsi               
 
         ; strcmp(targetName, namePtr)
         call    strcmp
         test    eax, eax
-        jz      ffbn_found                   ; 같다면 성공
+        jz      ffbn_found             
 
         inc     r12d
         jmp     ffbn_search_loop
 
 ffbn_found:
-        ; ordinal = ((WORD*)AddressOfNameOrdinals)[i]
-        movzx   eax, WORD PTR rdi + r12*2  ; eax = ordinal (index)
+        
+        movzx   eax, WORD PTR rdi + r12*2 
 
-        ; funcVA = base + ((DWORD*)AddressOfFunctions)[ordinal]
+       
         mov     edx, DWORD PTR r14+28      ; AddressOfFunctions RVA
         lea     rdx, [rbx+rdx]               ; rdx = AddressOfFunctions (VA)
         mov     eax, DWORD PTR rdx + rax*4 ; eax = Function RVA
         test    eax, eax
-        jz      ffbn_not_found               ; (희귀) 0이면 실패 취급
+        jz      ffbn_not_found              
 
         lea     rax, [rbx+rax]               ; rax = 실제 함수 주소
         jmp     ffbn_exit
 
 ffbn_not_found:
-        xor     rax, rax                     ; 실패 시 0
+        xor     rax, rax                   
 
 ffbn_exit:
         add     rsp, 32
@@ -129,18 +117,14 @@ ffbn_exit:
         pop     rbp
         ret
 
-; ------------------------------------------------------------
-; int strcmp(const char* s1 /*RCX*/, const char* s2 /*RDX*/)
-;   - 같으면 0, 다르면 1 반환 (간단 비교)
-; ------------------------------------------------------------
 strcmp:
 strcmp_loop:
         mov     al,  BYTE PTR rcx
-        mov     r8b, BYTE PTR rdx         ; r8b 사용: 비보존 RBX 건드리지 않기 위함
+        mov     r8b, BYTE PTR rdx         
         cmp     al, r8b
         jne     strcmp_not_equal
 
-        test    al, al                      ; NUL?
+        test    al, al                      
         jz      strcmp_equal
 
         inc     rcx
@@ -148,11 +132,11 @@ strcmp_loop:
         jmp     strcmp_loop
 
 strcmp_equal:
-        xor     eax, eax                    ; 0 (같음)
+        xor     eax, eax                    
         ret
 
 strcmp_not_equal:
-        mov     eax, 1                      ; 1 (다름)
+        mov     eax, 1                      
         ret
 
 ;-------------------------------------
@@ -165,3 +149,4 @@ _str:
 	DB ".\\bridge64.dll",0
 
 code_size = $-code_start
+
