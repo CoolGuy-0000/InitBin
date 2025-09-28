d6d82226b1c44352158fd146050939b3ca86c806
CoolGuy <84420090+CoolGuy-0000@users.noreply.github.com>
2025-09-28 16:44:27 +0900

    Update and rename initbin_test01.asm to initbin.asm

--- initbin.asm
+++ initbin.asm
@@ -4,22 +4,55 @@ ENTRY code_start
 
 section 'code' code readable writeable executable
 
-DD code_size
-
 code_start = $
 
 call GetCurrentAddress
+
+DB 0Fh, 1Fh, 84h, 00h
+_param01 = $-code_start
+DD code_size
+
 mov rbp, rax
 
 call GetKernelModuleHandle
+mov QWORD PTR rbp+KERNEL32, rax
 
-lea rdx, [rbp+_str.LoadLibraryA]
-mov rcx, rax
+lea rdx, [rbp+_str.GetCurrentThread]
+mov rcx, QWORD PTR rbp+KERNEL32
 call FindFuncByName
+call rax
+
+lea rdx, [rbp+_str.NUM_BUF01]
+mov rcx, rax
+call UInt64ToString
+
+lea rdx, [rbp+_str.NUM_BUF02]
+mov rcx, rbp
+call UInt64ToString
+
+lea rdx, [rbp+_str.SetEnvironmentVariableA]
+mov rcx, QWORD PTR rbp+KERNEL32
+call FindFuncByName
+mov QWORD PTR rbp+SetEnvironmentVariableA, rax
 
 sub rsp, 18h
+mov rdx, rdi
+
+lea rdx, [rbp+_str.NUM_BUF01]
+lea rcx, QWORD PTR rbp+_str.CG_VAR_hThread
+call QWORD PTR rbp+SetEnvironmentVariableA
+
+lea rdx, [rbp+_str.NUM_BUF02]
+lea rcx, QWORD PTR rbp+_str.CG_VAR_BaseRBP
+call QWORD PTR rbp+SetEnvironmentVariableA
+
+lea rdx, [rbp+_str.LoadLibraryA]
+mov rcx, QWORD PTR rbp+KERNEL32
+call FindFuncByName
+
 lea rcx, [rbp+_str.BridgePath]
 call rax
+
 add rsp, 18h
 
 L_self_loop: jmp L_self_loop
@@ -81,7 +114,6 @@ ffbn_search_loop:
         lea     rdx, [rbx+rax]         
         mov     rcx, rsi               
 
-        ; strcmp(targetName, namePtr)
         call    strcmp
         test    eax, eax
         jz      ffbn_found             
@@ -100,7 +132,7 @@ ffbn_found:
         test    eax, eax
         jz      ffbn_not_found              
 
-        lea     rax, [rbx+rax]               ; rax = 실제 함수 주소
+        lea     rax, [rbx+rax]
         jmp     ffbn_exit
 
 ffbn_not_found:
@@ -139,14 +171,99 @@ strcmp_not_equal:
         mov     eax, 1                      
         ret
 
+UInt64ToString:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push rsi
+    push rdi
+    push r12
+    push r13
+    push r14
+    push rcx
+
+    mov r8, rcx
+    mov r9, rdx
+    lea rdi, [rdx+15]
+    mov byte ptr rdi+1, 0
+    mov rax, 16
+    mov rcx, 16
+
+convert_loop:
+    mov r10d, 0Fh
+    and r10d, r8d
+
+    cmp r10d, 9
+    jle num_0_9
+    add r10d, 'A' - 10
+    jmp store_char
+
+num_0_9:
+    add r10d, '0'
+
+store_char:
+    mov [rdi], r10b
+    dec rdi
+    shr r8, 4
+    dec rcx
+    jnz convert_loop
+
+    mov rax, 16
+
+    pop rcx
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rsi
+    pop rbx
+    pop rbp
+    ret
 ;-------------------------------------
+
+
+KERNEL32 = $-code_start
+	DQ 0
+
+LoadLibraryA = $-code_start
+	DQ 0
+
+SetEnvironmentVariableA = $-code_start
+	DQ 0
 
 _str:
 	.LoadLibraryA = $-code_start
 	DB "LoadLibraryA",0
+	
+	.GetCurrentThread = $-code_start
+	DB "GetCurrentThread",0
+	
+	.SetEnvironmentVariableA = $-code_start
+	DB "SetEnvironmentVariableA",0
+	
+	.CG_VAR_hThread = $-code_start
+	DB "CG_VAR_hThread",0
+	
+	.CG_VAR_BaseRBP = $-code_start
+	DB "CG_VAR_BaseRBP",0
 
 	.BridgePath = $-code_start
-	DB ".\\bridge64.dll",0
+	DB ".\\bridge.dll",0
+    
+	DB 0
+	DB 0
+	DB 0
+	DB 0
+	DB 0
+	DB 0
+
+	.NUM_BUF01 = $-code_start
+	times 32 DB 0
+
+	.NUM_BUF02 = $-code_start
+	times 32 DB 0
+
+	DD 0CCCCCCCCh
+	DD 0CCCCCCCCh
 
 code_size = $-code_start
-
